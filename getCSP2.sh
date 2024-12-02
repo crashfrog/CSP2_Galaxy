a8eb67e4da3ca9b100d92f6a7493b843d54104fd
Bob Literman <jazz710@gmail.com>
2024-12-02 09:39:07 -0500

    Update getCSP2.sh
    Simplify

--- getCSP2.sh
+++ getCSP2.sh
@@ -1,5 +1,2 @@
 git clone --branch main --single-branch https://github.com/CFSAN-Biostatistics/CSP2.git
-cd CSP2
-git checkout v.0.9.7.1
-cd ..
 nextflow run CSP2/CSP2.nf -profile standard_conda --runmode conda_init
